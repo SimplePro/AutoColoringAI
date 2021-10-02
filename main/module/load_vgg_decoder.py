55051e1e7e5815d188b09d86c443560fd5b9c254
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-10-02 22:34:34 +0900

    add load vgg decoder python module file

---
+++ main/module/load_vgg_decoder.py
@@ -0,0 +1,108 @@
+import torch.nn as nn
+import torch
+
+def load(vgg_path, decoder_path, device):
+    # 인코더(Encoder) 정의
+    vgg = nn.Sequential(
+        nn.Conv2d(3, 3, (1, 1)),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(3, 64, (3, 3)),
+        nn.ReLU(), # relu1-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(), # relu1-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 128, (3, 3)),
+        nn.ReLU(), # relu2-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(), # relu2-2
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 256, (3, 3)),
+        nn.ReLU(), # relu3-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(), # relu3-2
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(), # relu3-3
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(), # relu3-4
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 512, (3, 3)),
+        nn.ReLU(), # relu4-1, this is the last layer used
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU(), # relu4-2
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU(), # relu4-3
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU(), # relu4-4
+        nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU(), # relu5-1
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU(), # relu5-2
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU(), # relu5-3
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 512, (3, 3)),
+        nn.ReLU() # relu5-4
+    )
+
+    # 디코더(Decoder) 정의
+    decoder = nn.Sequential(
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(512, 256, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 256, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(256, 128, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 64, (3, 3)),
+        nn.ReLU(),
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, (3, 3)),
+        nn.ReLU(),
+        nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 3, (3, 3)),
+    )
+
+    # 사전학습된 vgg network와 decoder network의 가중치를 실제 사용할 모델에 로드한다.
+
+    decoder.eval()
+    vgg.eval()
+
+    vgg.load_state_dict(torch.load(vgg_path))
+    decoder.load_state_dict(torch.load(decoder_path))
+
+    vgg.to(device)
+    decoder.to(device)
+
+    vgg = nn.Sequential(*list(vgg.children())[:31])
+
+    return vgg, decoder
