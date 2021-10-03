73dc4445ebb1e20e65c83c99918afffdcd4c16a1
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-10-03 22:09:45 +0900

    add program python file

--- main/program.py
+++ main/program.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, QLabel
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton
+from PyQt5.QtGui import QIcon
 
 
 class MyApp(QWidget):
@@ -16,42 +16,45 @@ class MyApp(QWidget):
         self.setFixedSize(1000, 600)
         self.center()
 
-
         # 채색 버튼
         self.painting_btn = QPushButton("", self)
         self.painting_btn.setStyleSheet("image : url(C:/kimdonghwan/python/auto_coloring_ai/main/ui_imgs"
-                                        "/painting_btn_img.png);" "border-radius: 0px")
+                                        "/painting_btn_img.png);" "border-radius: 0px;")
         self.painting_btn.move(10, 10)
         self.painting_btn.resize(140, 140)
         self.painting_btn.clicked.connect(self.clicked_painting)
 
-
         # 스케치 찾아보기 버튼
         self.explorer_btn = QPushButton("", self)
         self.explorer_btn.setStyleSheet("image : url(C:/kimdonghwan/python/auto_coloring_ai/main/ui_imgs"
-                                        "/explorer_btn_img);" "border-radius: 0px")
+                                        "/explorer_btn_img);" "border-radius: 0px;")
         self.explorer_btn.move(170, 10)
         self.explorer_btn.resize(140, 40)
-
         self.explorer_btn.clicked.connect(self.clicked_explorer)
 
         # 미술 작품 추가하기 버튼
         self.add_art_btn = QPushButton("", self)
         self.add_art_btn.setStyleSheet("image : url(C:/kimdonghwan/python/auto_coloring_ai/main/ui_imgs"
-                                        "/add_art_btn_img.png);" "border-radius: 0px")
+                                        "/add_art_btn_img.png);" "border-radius: 0px;")
         self.add_art_btn.move(170, 60)
         self.add_art_btn.resize(310, 50)
-
         self.add_art_btn.clicked.connect(self.clicked_add_art)
 
         # 채색 결과 확인 버튼
         self.check_result_btn = QPushButton("", self)
         self.check_result_btn.setStyleSheet("image : url(C:/kimdonghwan/python/auto_coloring_ai/main/ui_imgs"
-                                       "/check_result_btn_img.png);" "border-radius: 0px")
+                                       "/check_result_btn_img.png);" "border-radius: 0px;")
         self.check_result_btn.move(170, 120)
         self.check_result_btn.resize(410, 90)
-
         self.check_result_btn.clicked.connect(self.clicked_check_result)
+
+        # 저장 버튼
+        self.save_btn = QPushButton("", self)
+        self.save_btn.setStyleSheet("image : url(C:/kimdonghwan/python/auto_coloring_ai/main/ui_imgs"
+                                            "/save_btn_img.png);" "border-radius: 0px;")
+        self.save_btn.move(10, 160)
+        self.save_btn.resize(120, 120)
+        self.save_btn.clicked.connect(self.clicked_save)
 
         self.show()
 
@@ -76,6 +79,10 @@ class MyApp(QWidget):
     # 채색 결과 확인 버튼이 클릭됐을 때 실행되는 메소드.
     def clicked_check_result(self):
         print("clicked check result btn")
+
+    # 저장 버튼이 클릭됐을 때 실행되는 메소드.
+    def clicked_save(self):
+        print("clicked clicked save btn")
 
 
 if __name__ == '__main__':
