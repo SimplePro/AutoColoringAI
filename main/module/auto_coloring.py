db1c85cd383e6d46bd13453987a341333cdb7f7d
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-10-03 12:54:26 +0900

    add auto coloring python module file

--- main/module/auto_coloring.py
+++ main/module/auto_coloring.py
@@ -4,42 +4,41 @@ from torchvision import transforms
 import numpy as np
 from PIL import Image
 import load_vgg_decoder
-import os
 
+import warnings
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-vgg, decoder = load_vgg_decoder.load(vgg_path=f"{os.getcwd()}\\main\\pre-trained\\vgg_normalised.pth",
-                                    decoder_path=f"{os.getcwd()}\\main\\pre-trained\\decoder.pth", device=device)
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
 class AutoColoring:
-    def __init__(self, content_path, style_paths, weights):
-        self.content_img = Image.open(content_path).convert("RGB").resize((128, 128))
-        self.style_imgs = [Image.open(style_path).convert("RGB").resize((128, 128)) for style_path in style_paths]
 
+    def __init__(self, content_path, style_paths, size, weights):
+        self.content_img = Image.open(content_path).convert("RGB").resize(size)
+        self.style_imgs = [Image.open(style_path).convert("RGB").resize(size) for style_path in style_paths]
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.vgg, self.decoder = load_vgg_decoder.load(vgg_path="../pre-trained/vgg_normalised.pth",
+                                                       decoder_path="../pre-trained/decoder.pth",
+                                                       device=device)
+
+        self.size = size
         self.weights = weights
-        
-        
-    # this method return result after preprocess
-    # shape of (1, 3, 128, 128) -> shape of (128, 128, 3)
+
     def decode3shape(self, coloring_result):
         coloring_result = coloring_result[0]
-        
-        result = np.zeros((128, 128, 3))
-        
-        for i in range(128):
-            for j in range(128):
+
+        result = np.zeros((*self.size, 3))
+
+        for i in range(self.size[0]):
+            for j in range(self.size[0]):
                 result[i, j] = coloring_result[:, i, j]
-                
+
         return result
-    
-    
-    # this method return the transform object
+
     def transform(self):
         return transforms.Compose([transforms.ToTensor()])
-    
-    
-    # this method return the features statistics of mean and standard-deviation.
+
     def style_(self, feature, eps=1e-5):
         N, C = feature.size()[:2]
 
@@ -49,10 +48,7 @@ class AutoColoring:
         std = (feature.var(dim=2) + eps).sqrt().reshape(N, C, 1, 1)
 
         return mean, std
-    
-    
-    # adain_normalization
-    # style transfer
+
     def adain_normalization(self, content_f, style_f):
         size = content_f.size()
 
@@ -66,32 +62,47 @@ class AutoColoring:
         normalized_f = style_std * normalized_f + style_mean
 
         return normalized_f
-    
-    
-    # multiple style transfer
+
     def interpolation(self, content, styles):
-        content_f = vgg(content)
-        style_fs = [vgg(style) for style in styles]
+        content_f = self.vgg(content)
+        style_fs = [self.vgg(style) for style in styles]
 
-        feature = sum([self.weights[i] * self.adain_normalization(content_f, style_fs[i]) for i in range(len(style_fs))])
+        feature = sum(
+            [self.weights[i] * self.adain_normalization(content_f, style_fs[i]) for i in range(len(style_fs))])
 
-        return decoder(feature)
-    
-    
-    # this method return the result after AutoColoring
+        return self.decoder(feature)
+
     def result_(self):
-        content = self.transform()(self.content_img).to(device).unsqueeze(0)
+        content = self.transform()(self.content_img).to(self.device).unsqueeze(0)
         styles = []
         for i in range(len(self.style_imgs)):
-            styles.append(self.transform()(self.style_imgs[i]).to(device).unsqueeze(0))
-            
+            styles.append(self.transform()(self.style_imgs[i]).to(self.device).unsqueeze(0))
+
         coloring_result = self.interpolation(content, styles)
-                            
+
         torch.cuda.empty_cache()
         gc.collect()
-                                        
+
         coloring_result = coloring_result.cpu().detach().numpy()
-        
+
         coloring_result = self.decode3shape(coloring_result)
-        
+
         return coloring_result
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import os
+
+    file_path = os.path.abspath("./")[:-6]
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    auto_coloring = AutoColoring(content_path=f"{file_path[:-5]}\\test_image\\content.jpg",
+                                 style_paths=[f"{file_path[:-5]}\\test_image\\style_house.jpg"],
+                                 size=(128, 128), weights=[1.0])
+
+    coloring_result = auto_coloring.result_()
+
+    plt.imshow(coloring_result)
+    plt.show()
